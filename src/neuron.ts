0fc796627e7d7dc246e666ee2f1cd5c1f9707aec
Mingzhang <redondoself@gmail.com>
2018-04-10 20:45:15 -0400

    Update neuron.ts
    change the range of weights from [0, 1) to (-1, 1);
    add bias to Neuron class

--- src/neuron.ts
+++ src/neuron.ts
@@ -1,12 +1,14 @@
 class Neuron {
     numOfWeights: number;
     weights: number[];
+    bias: number;
     constructor(n: number) {
         this.numOfWeights = n;
         this.weights = (new Array<number>(n));
         for (let i = 0; i < this.weights.length; i++) {
-            this.weights[i] = Math.random();
+            this.weights[i] = (Math.random() - 0.5) * 2;
         }
+        this.bias = Math.random();
     }
     // given an input vector, compute the result
     compute(input: number[]): number {
